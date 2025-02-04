8635cf3f18a6e5f60a5ccfe0c5feacd3618ca445
Mohd Parvez Ansari <mdparvezansari91@gmail.com>
2025-02-04 11:25:58 +0400

    cors installed

--- backendnodejs/index.js
+++ backendnodejs/index.js
@@ -2,6 +2,10 @@ const express = require("express")
 const app = express()
 require("dotenv").config();
 const PORT = process.env.PORT || 8000
+const cors = require("cors");
+
+app.use(cors());
+
 
 app.get("/",(req,res)=>{
 
