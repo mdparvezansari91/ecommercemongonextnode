a0ee5d4dcea4d4ec98de0381f8f4fbebee3f95da
Mohd Parvez Ansari <mdparvezansari91@gmail.com>
2025-02-04 15:59:43 +0400

    Update userSignin.js

--- backendnodejs/controllers/userSignin.js
+++ backendnodejs/controllers/userSignin.js
@@ -39,8 +39,8 @@ const signIn =async (req, res) => {
 
         const token = jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, {expiresIn: 60*60*8 })
         res.cookie('token', token, { 
-            httpOnly: true, 
-            secure: true,
+            HttpOnly: true, 
+            Secure: true,
             SameSite:"None",
         }); // Use secure cookies in production
         res.setHeader('Set-Cookie', 'myCookie=cookie_value; Max-Age=3600');
