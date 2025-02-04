e7a5fb5c672ee165e15f1a2150324730d2074b3b
Mohd Parvez Ansari <mdparvezansari91@gmail.com>
2025-02-04 11:05:33 +0400

    test hello world

--- backendnodejs/index.js
+++ backendnodejs/index.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const app = express()
 
-
+const PORT = process.env.PORT || 8000
 
 app.get("/",(req,res)=>{
-    
+
     res.send("Hello World")
 })
 
-app.listen(8000, ()=>{
+app.listen(PORT, ()=>{
     console.log("server is running on port 8000")
 })
