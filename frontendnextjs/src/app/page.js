451fe863a2170ccca577451f15277b0eb47506d7
Mohd Parvez Ansari <mdparvezansari91@gmail.com>
2025-02-05 09:02:48 +0400

    Update page.js

--- frontendnextjs/src/app/page.js
+++ frontendnextjs/src/app/page.js
@@ -1,10 +1,12 @@
 'use client'
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { userCheck } from '@/store/user/userSlice';
 
 export default function Home() {
+  const user = useSelector(state=>state.user.user)
+  console.log(user?.userId?.emailid);
   const dispatch = useDispatch();
   useEffect(()=>{
     dispatch(userCheck())
@@ -14,6 +16,8 @@ export default function Home() {
     <>
       <div>
         <div>
+          <div>{user?.userId?.emailid}</div>
+          
           <Link href={"/signin"}>Go to Signin</Link>
         </div>
       </div>
